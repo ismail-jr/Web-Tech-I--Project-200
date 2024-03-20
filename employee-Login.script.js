6b5365e1d3f8a446ce8bc10d49ea4dc1fd63b277
ismail-jr <ismail.jibriel.jr@gmail.com>
2024-03-20 09:54:35 +0000

    Web Technology I project Work

---
+++ employee-Login.script.js
@@ -0,0 +1,8 @@
+function submitLoginForm(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+
+    alert("Username:" + username + "\nPassword:" + password + "\nEmail:" + email);
+    
+}
